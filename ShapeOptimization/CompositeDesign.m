315d9e39f7c1d932c72ab6dcaa69a7a9c035c264
Elmar Puts <elmarputs@gmail.com>
2019-04-20 16:29:27 +0200

    Added code for response surface and goodness of fit

--- ShapeOptimization/CompositeDesign.m
+++ ShapeOptimization/CompositeDesign.m
@@ -17,36 +17,22 @@ fitness = ccdResults(:, 8:9);
 fitness(:,1) = fitness(:,1)./1e9;
 fitness(:,2) = fitness(:,2)./1e6;
 
-%x = linspace(min(xValues), max(xValues), 50);
-%y = linspace(min(yValues), max(yValues), 50);
-%[X, Y] = meshgrid(x, y);
-%Z = griddata(xValues, yValues, fitness, X, Y);
+rstool(decisionVariables, fitness, 'quadratic')
 
-%figure;
-%surfc(X, Y, Z);
+%% Compute R² (goodness of fit)
 
-%% Construct response surface
+Sr1 = sumsqr(residuals(:,1));
+Sr2 = sumsqr(residuals(:,2));
 
-% Perform least squares regression
-A = [ones(length(xValues),1) xValues yValues xValues.^2 yValues.^2 xValues.*yValues.^2 xValues.^2.*yValues xValues.^3.*yValues xValues.*yValues.^3 xValues.^4 yValues.^4];
-b = lsqr(A, fitness);
+dev1 = fitness(:,1) - mean(fitness(:,1));
+dev2 = fitness(:,2) - mean(fitness(:,2));
+St1 = sumsqr(dev1);
+St2 = sumsqr(dev2);
 
-i = 1;
-for xV = x
-    j = 1;
-    for yV = y
-        f(i,j) = [1 xV yV xV^2 yV^2 xV*yV^2 xV^2*yV xV^3*yV xV*yV^3 xV^4 yV^4] * b;
-        j = j + 1;
-    end
-    i = i + 1;
-end
+R1 = 1 - Sr1/St1;
+R2 = 1 - Sr2/St2;
 
-figure;
-surfc(X, Y, f);
-
-% Compute goodness of fit
-fitnessMean = mean(fitness);
-fitValues = A*b;
-Sr = sumsqr(fitValues - fitnessMean);
-St = sumsqr(fitness - fitnessMean);
-R_sqr = Sr/St;
+N = length(fitness(:,1));
+p = length(beta);
+Ra1 = 1-(1-R1)*((N-1)/(N-p));
+Ra2 = 1-(1-R2)*((N-1)/(N-p));
